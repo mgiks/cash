600701ab728ec87f983161cbc19b1b71b202a847
mgiks <lidamil6785@gmail.com>
2025-10-17 01:57:50 +0600

    feat: add function for arena allocation

--- include/arena.h
+++ include/arena.h
@@ -11,4 +11,7 @@ typedef struct {
 
 void ArenaInit(Arena *arena, uint64_t buffer_size);
 
+void *ArenaAlloc(Arena *arena, uint64_t num_of_elems, uint64_t elem_size,
+                 uint64_t align_size);
+
 #endif
--- src/arena.c
+++ src/arena.c
@@ -2,6 +2,7 @@
 
 #include <stdint.h>
 #include <stdlib.h>
+#include <string.h>
 
 void ArenaInit(Arena *arena, uint64_t buffer_size) {
     *arena = (Arena){
@@ -9,4 +10,34 @@ void ArenaInit(Arena *arena, uint64_t buffer_size) {
         .bufferSize = buffer_size,
         .currentOffset = 0,
     };
+}
+
+void *ArenaAlloc(Arena *arena, uint64_t num_of_elems, uint64_t elem_size,
+                 uint64_t align_size) {
+    // Alignment must be a power of two
+    if (align_size == 0 || (align_size & (align_size - 1)) != 0) {
+        return NULL;
+    }
+
+    uintptr_t allocation_size = num_of_elems * elem_size;
+
+    // Overflow check
+    if (num_of_elems != 0 && allocation_size / num_of_elems != elem_size) {
+        return NULL;
+    }
+
+    uintptr_t total_offset =
+        (uintptr_t)arena->buffer + (uintptr_t)arena->currentOffset;
+    uintptr_t padding = total_offset % align_size;
+
+    total_offset += padding;
+
+    if (total_offset + allocation_size >
+        (uintptr_t)arena->buffer + (uintptr_t)arena->bufferSize) {
+        return NULL;
+    }
+
+    arena->currentOffset += (padding + allocation_size);
+    memset((void *)total_offset, 0, allocation_size);
+    return (void *)total_offset;
 }
